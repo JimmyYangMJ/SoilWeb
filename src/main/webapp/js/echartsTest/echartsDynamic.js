e42c54fff55190ccbccedbd78068a268fba980d2
YMj1998 <2237895205@qq.com>
2019-11-28 11:49:11 +0800

    v1.1 add insertSoilNode

---
+++ src/main/webapp/js/echartsTest/echartsDynamic.js
@@ -0,0 +1,128 @@
+
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+function randomData() {
+    now = new Date(+now + oneDay);
+
+    value = value + Math.random() * 21 - 10;
+    return {
+        name: now.toString(),
+        value: [
+            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+            Math.round(value)
+        ]
+    }
+}
+
+var data = [];
+var now = +new Date(1997, 9, 3);
+var oneDay = 24 * 3600 * 1000;
+var value = Math.random() * 1000;
+for (var i = 0; i < 50; i++) {
+    data.push(randomData());
+}
+
+option = {
+    title: {
+        text: '土壤水势 动态数据',
+        subtext: 'sspu'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    toolbox: { // 工具栏
+        show: true,
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            dataView: {readOnly: false},
+            magicType: {type: ['line', 'bar']},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    legend: {
+        data:['结点 1','结点 2']
+    },
+    dataZoom: [
+        {
+            show: false,
+            start: 0,
+            end: 100,
+            type: 'inside'
+        },
+        {
+            id: 'dataZoomX',
+            type: 'slider',
+            xAxisIndex: [0],
+            filterMode: 'filter'
+        },
+        {
+            id: 'dataZoomY',
+            type: 'slider',
+            yAxisIndex: [0],
+            filterMode: 'empty'
+        }
+    ],
+    xAxis: {
+        type: 'time',
+        boundaryGap: true,
+
+    },
+    yAxis: {
+        type: 'value',
+        //boundaryGap: [0, '100%'],
+        boundaryGap: [0.2, 0.2],
+        scale: true,
+        name: '水势',
+        //min: 0,
+        axisLabel: {
+            formatter: '{value} 巴'
+        }
+
+    },
+    series: [{
+        name: '结点 1',
+        type: 'line',
+        showSymbol: true, // 是否显示 symbol
+        hoverAnimation: false,
+        symbolSize: 6,   //折线点的大小
+        markPoint: {
+            data: [
+                {type: 'max', name: '最大值'},
+                {type: 'min', name: '最小值'}
+            ]
+        },
+        markLine: {
+            data: [
+                {type: 'average', name: '平均值'}
+            ]
+        },
+        data: data
+    }]
+};
+
+setInterval(function () {
+
+    data.push(randomData());
+
+    myChart.setOption({
+        series: [{
+            data: data
+        }]
+    });
+}, 2100);;
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
---
+++ src/main/webapp/js/echartsTest/echartsStatic.js
@@ -0,0 +1,130 @@
+
+    function generateChart(data) {
+
+        var dom = document.getElementById("echart");
+        var myChart = echarts.init(dom);
+        // 加载数据
+        course = data;
+        var status = course.status;
+
+        if(status == 1){  //  没有相应记录
+            alert("没有相应记录");
+        }
+        var node = course.data;
+        var nodeId = node[0].node;
+        nodeId = "结点 " + nodeId;
+        var nodeTime = new Array();
+        var nodeHumidity = new Array();
+        for (var i = 0; i < node.length; i++) {
+            nodeTime[i] = node[i].times;
+            nodeHumidity[i] = node[i].humidity;
+        }
+        //
+        option = {
+            title : {
+                text: '土壤水势',
+                subtext: course.msg
+            },
+            tooltip : {
+                trigger: 'axis'
+            },
+            legend: {
+                data:[nodeId, '节点二']
+            },
+            dataZoom:[
+                {
+                    show: true,
+                    start: 0,
+                    end: 100,
+                    type: 'inside'
+                },
+                {
+                    type: 'slider',//图表下方的伸缩条
+                    show : true, //是否显示
+                    realtime : true, //拖动时，是否实时更新系列的视图
+                    start : 0, //伸缩条开始位置（1-100），可以随时更改
+                    end : 100, //伸缩条结束位置（1-100），可以随时更改
+                }
+            ],
+            toolbox: {
+                show : true,
+                feature : {
+                    mark : {show: true},
+                    dataView : {show: true, readOnly: false},
+                    magicType : {show: true, type: ['line', 'bar']},
+                    restore : {show: true},
+                    saveAsImage : {show: true}
+                }
+            },
+            calculable : true,
+            xAxis : [
+                {
+                    type : 'category',
+                    boundaryGap : true,
+                    data: nodeTime
+
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value',
+                    axisLabel : {
+                        formatter: '{value} pa'
+                    }
+                }
+            ],
+            series : [
+                {
+                    name: nodeId,
+                    type:'line',
+                    data: nodeHumidity,
+                    // data:[11, 50, 15, 13, 12, 13, 10,9],
+                    markPoint : {
+                        data : [
+                            {type : 'max', name: '最大值'},
+                            {type : 'min', name: '最小值'}
+                        ]
+                    },
+                    markLine : {
+                        data : [
+                            {type : 'average', name: '平均值'}
+                        ]
+                    }
+                },
+                {
+                    name:'节点二',
+                    type:'line',
+                    data:[1, -2, 2, 5, 3, 2, 0,4],
+                    markPoint : {
+                        data : [
+                            {name : '周最低', value : -2, xAxis: 1, yAxis: -1.5}
+                        ]
+                    },
+                    markLine : {
+                        data : [
+                            {type : 'average', name : '平均值'}
+                        ]
+                    }
+                }
+            ]
+        };
+
+        // 为echarts对象加载数据
+        myChart.setOption(option, window.onresize = myChart.resize);
+    }
+
+//buttion调用的方法
+function getData() {
+    var node = $('#node').val();
+    var day = $('#day').val();
+    alert("结点："+node + " 时间：" + day);
+    //异步请求
+    $.get(  // jQuery post() 方法
+        "soil/selectSoilSet.do", //访问地址
+        {node: node, day: day}, //携带的参数
+        function (data) {
+            generateChart(data);
+        },
+        "json"
+    );
+}
